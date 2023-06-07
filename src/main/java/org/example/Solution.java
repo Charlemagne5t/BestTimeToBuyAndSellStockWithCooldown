9d1d01cb2e7a865489714fa2efd6a0d6cbf26822
alek <wait82985@gmail.com>
2023-06-07 16:28:39 +0600

    recursive func seems to work but gives TLE on large prices length. Memo needed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,11 +4,24 @@ import java.util.Arrays;
 
 public class Solution {
     public int maxProfit(int[] prices) {
+        boolean ableToBuy = true; // false == able to sell
 
-        int[] dp = new int[prices.length];
-        dp[0] = 0;
 
-        System.out.println(Arrays.toString(dp));
-        return 0;
+        return dfs(prices, 0, true, 0);
     }
+    private int dfs(int[] prices, int index, boolean ableToBuy, int total){
+        if(index >= prices.length) return total;
+        int result = 0;
+        for (int i = index; i < prices.length; i++) {
+            if(ableToBuy){
+                result = Math.max(dfs(prices, index + 1, false, total - prices[index]), dfs(prices, index + 1, ableToBuy, total));
+            }
+            if(!ableToBuy){
+                result = Math.max(dfs(prices, index + 2, true, total + prices[index]), dfs(prices, index + 1, false, total));
+            }
+        }
+
+        return result;
+    }
+
 }
