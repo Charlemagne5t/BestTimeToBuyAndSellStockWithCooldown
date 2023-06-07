4beb4f04fb788fa0e06e06caa798edd3f598d1c7
alek <wait82985@gmail.com>
2023-06-07 17:29:45 +0600

    problem solved. DP Top-down.

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,34 +1,35 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        boolean ableToBuy = true; // false == able to sell
-        int[][] memo = new int[prices.length][2];
-        int[] maxResult = {0};
-        dfs(prices, 0, true, 0, memo, maxResult);
-        return maxResult[0];
+        Map<String, Integer> memo = new HashMap<>();
+
+        return dfs(prices, 0, true, memo);
     }
 
-    private int dfs(int[] prices, int index, boolean ableToBuy, int total, int[][] memo, int[] maxResult) {
+    private int dfs(int[] prices, int index, boolean ableToBuy, Map<String, Integer> memo) {
         if (index >= prices.length) {
-            maxResult[0] = Math.max(maxResult[0], total);
-            return total;
-        }
-        int result = 0;
-        if (ableToBuy) {
-            int buy = dfs(prices, index + 1, false, total - prices[index], memo, maxResult);
-            int skip = dfs(prices, index + 1, true, total, memo, maxResult);
-            result = Math.max(buy, skip);
-        }
-        if (!ableToBuy) {
-            int sell = dfs(prices, index + 2, true, total + prices[index], memo, maxResult);
-            int skip = dfs(prices, index + 1, false, total, memo, maxResult);
-            result = Math.max(sell, skip);
+            return 0;
         }
 
-        System.out.println(result);
-        return result;
+        if (memo.containsKey(index + " " + ableToBuy)) {
+            return memo.get(index + " " + ableToBuy);
+        }
+
+        if (ableToBuy) {
+            int buy = dfs(prices, index + 1, false, memo) - prices[index];
+            int skip = dfs(prices, index + 1, true, memo);
+            memo.put(index + " " + ableToBuy, Math.max(buy, skip));
+        }
+        if (!ableToBuy) {
+            int sell = dfs(prices, index + 2, true, memo) + prices[index];
+            int skip = dfs(prices, index + 1, false, memo);
+            memo.put(index + " " + ableToBuy, Math.max(sell, skip));
+        }
+
+        return memo.get(index + " " + ableToBuy);
     }
 }
