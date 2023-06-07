ca131c71a496c3efdb9c78c49d9c4279c60b6cfb
alek <wait82985@gmail.com>
2023-06-07 14:26:41 +0600

    initial commit, 2 test cases

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+
+        int[] dp = new int[prices.length];
+        dp[0] = 0;
+
+        System.out.println(Arrays.toString(dp));
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void maxProfitTest1() {
+        int[] prices = {1, 2, 3, 0, 2};
+        int output = 3;
+        Assert.assertEquals(output, new Solution().maxProfit(prices));
+    }
+
+    @Test
+    public void maxProfitTest2() {
+        int[] prices = {1};
+        int output = 0;
+        Assert.assertEquals(output, new Solution().maxProfit(prices));
+    }
+
+}
